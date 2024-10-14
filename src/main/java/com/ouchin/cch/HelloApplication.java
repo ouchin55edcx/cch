591ac290ef27c06f85dda4f56304157f37114730
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-10-14 11:30:06 +0100

    folder structure

---
+++ src/main/java/com/ouchin/cch/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ouchin.cch;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
